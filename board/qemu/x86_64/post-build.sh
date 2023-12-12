502390ac0aa25e7f90177d31e41a4f5b257f5146
Danila Pechenev <d.pechenev@yadro.com>
2023-12-12 20:07:06 +0300

    Automatically build klish config

--- board/qemu/x86_64/post-build.sh
+++ board/qemu/x86_64/post-build.sh
@@ -9,3 +9,10 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # QEMU graphical window' ${TARGET_DIR}/etc/inittab
 fi
+
+KLISH_PATH=${BASE_DIR}/../klish
+cp -r ${KLISH_PATH}/clish ${TARGET_DIR}/etc
+cp -r ${KLISH_PATH}/clish-enable ${TARGET_DIR}/etc
+cp ${KLISH_PATH}/init.d/klish-init "${TARGET_DIR}/etc/init.d/S50klish-init"
+cp ${KLISH_PATH}/startup-config "${TARGET_DIR}/etc/startup-config"
+
